609184ac2a60f37df35aa9435c679cf8ba15f0b3
Michael Carlos <michaelpcarlos@gmail.com>
2018-05-29 19:03:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log('HELLO');
+}
+
+function logWhisper(string) {
+  console.log('hello');
+}
+
+function sayHiToGrandma(string) {
+  if (string.toUpperCase) {
+    return "YES INDEED!";
+  } else if (string.toLowerCase === 'hello') {
+    return "I can't hear you!";
+  } else {
+    return "I love you, Grandma.";
+  }
+}
